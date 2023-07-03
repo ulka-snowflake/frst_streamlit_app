07531d92fe0f080fe8132777a2357c907f95a591
ulka-snowflake <138463998+ulka-snowflake@users.noreply.github.com>
2023-07-03 15:47:39 +0530

    Update streamlit_app.py
    corrected font

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit
 streamlit.title("My parent's healthy diner")
 
 streamlit.header("Breakfast Menu")
-streamlit.title("Omega 3 and Blueberry Oatmeal")
-streamlit.title("Kale, Spinach and Rocket Smoothie")
-streamlit.title("Hard-boiled Free-Range Egg")
+streamlit.text("Omega 3 and Blueberry Oatmeal")
+streamlit.text("Kale, Spinach and Rocket Smoothie")
+streamlit.text("Hard-boiled Free-Range Egg")
