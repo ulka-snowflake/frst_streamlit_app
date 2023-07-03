e6f8778adbaf7b1a0637b94a2bbc799ade669cf3
ulka-snowflake <138463998+ulka-snowflake@users.noreply.github.com>
2023-07-03 15:46:59 +0530

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My parent's healthy diner")
+
+streamlit.header("Breakfast Menu")
+streamlit.title("Omega 3 and Blueberry Oatmeal")
+streamlit.title("Kale, Spinach and Rocket Smoothie")
+streamlit.title("Hard-boiled Free-Range Egg")
