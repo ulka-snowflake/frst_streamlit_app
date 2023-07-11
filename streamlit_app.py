32f0a029e966693ba663c3f57bc2df403cae16f9
ulka-snowflake <138463998+ulka-snowflake@users.noreply.github.com>
2023-07-11 14:43:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ except URLError as e:
   streamlit.error()
 streamlit.write('The user entered ', fruit_choice)
 
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favroites!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
@@ -49,8 +49,9 @@ def get_fruit_load_list():
 #add button to load the fruit
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  back_from_function = get_fruit_load_list()
-  streamlit.dataframe(back_from_function)
+  my_data_rows = get_fruit_load_list()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
 
 #Allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
