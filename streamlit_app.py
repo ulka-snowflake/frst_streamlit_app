957cab0d405cb4efe2059ce586ba234172ceee80
ulka-snowflake <138463998+ulka-snowflake@users.noreply.github.com>
2023-07-11 14:22:16 +0530

    Update streamlit_app.py
    optimize

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,16 +21,22 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+#new section to display fruitvice api
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_coice:
+    streamlit.error("Please select a fruit to get infromation")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
-#take the json version of the response and normalize 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output as table as dataframe
-streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
